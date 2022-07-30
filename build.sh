25ae0f7f96c9897974bbaa36d8325ca676d958a1
timurgordon <timurgordon@gmail.com>
2022-07-30 18:03:11 +0200

    build script compatible with linux & arm64

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
-# gets and builds executable if doesn't exist
+# gets and builds executable for os and cpu if doesn't exist
+echo "Installing & building tailwind"
 if [[ ! -f "tailwindcss" ]]
 then
-    curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-macos-x64
-    chmod +x tailwindcss-macos-x64
-    mv tailwindcss-macos-x64 tailwindcss
+    #https://stackoverflow.com/a/8597411 
+    ASSET="tailwindcss"
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        $ASSET = "$ASSET-linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        ASSET="$ASSET-macos"
+    fi
+    echo $(uname -m)
+    if [[ "$(uname -m)" == "x86_64"* ]]; then
+        ASSET="$ASSET-x64"
+    elif [[ "$(uname -m)" == "arm64"* ]]; then
+        ASSET="$ASSET-arm64"
+    fi
+    curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
+    chmod +x $ASSET
+    mv $ASSET tailwindcss
 fi
 
 # initialized and configures tailwind if not configured
