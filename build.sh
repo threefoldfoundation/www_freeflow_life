e7e23104d3309db71dafce0cf82ee45e319be142
timurgordon <timurgordon@gmail.com>
2022-07-31 13:59:32 +0200

    fix build and create start script

--- build.sh
+++ build.sh
@@ -1,40 +1,41 @@
-trap "killall background" EXIT
+echo "Starting build..."
 
-# gets and builds executable for os and cpu if doesn't exist
-echo "Installing & building tailwind"
-ls
-rm tailwindcss
-ls
-if [[ ! -f "tailwindcss" ]]
+# TODO: Check if current version is latest to avoid redundant installation
+if [[ -f "tailwindcss" ]]
 then
-    echo "Checking environment"
-    #https://stackoverflow.com/a/8597411 
-    ASSET="tailwindcss"
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        ASSET="$ASSET-linux"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        ASSET="$ASSET-macos"
-    fi
-    echo $(uname -m)
-    if [[ "$(uname -m)" == "x86_64"* ]]; then
-        ASSET="$ASSET-x64"
-    elif [[ "$(uname -m)" == "arm64"* ]]; then
-        ASSET="$ASSET-arm64"
-    fi
-
-    echo "Installing & building tailwind from ${ASSET}"
-    curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
-    chmod +x $ASSET
-    mv $ASSET tailwindcss
+    rm tailwindcss
 fi
 
+# checks os and architecture for correct release
+# https://stackoverflow.com/a/8597411 
+echo "Installing & building tailwind..."
+ASSET="tailwindcss"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ASSET="$ASSET-linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ASSET="$ASSET-macos"
+fi
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    ASSET="$ASSET-x64"
+elif [[ "$(uname -m)" == "arm64"* ]]; then
+    ASSET="$ASSET-arm64"
+fi
+
+curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
+chmod +x $ASSET
+mv $ASSET tailwindcss
+
+
 # initialized and configures tailwind if not configured
+echo "Initializing tailwind..."
 if [[ ! -f "tailwind.config.js" ]]
 then
     ./tailwindcss init
     sed -i '' "s|  content: \\[\\],|  content: \\['./templates/**/*.html'\\],|g" tailwind.config.js
 fi
 
+echo "Compiling tailwindcss and building zola project..."
 # compiles tailwind css for prod & builds project
 rm -rf public static/css
 ./tailwindcss -i css/index.css -o ./static/css/index.css --minify
