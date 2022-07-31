e2aa1aa8604d3f10756a9fc5545c7c41d6a72d02
timurgordon <timurgordon@gmail.com>
2022-07-31 09:30:35 +0200

    add binary to gitignore

--- build.sh
+++ build.sh
@@ -27,12 +27,8 @@ then
     sed -i '' "s|  content: \\[\\],|  content: \\['./templates/**/*.html'\\],|g" tailwind.config.js
 fi
 
-# compiles tailwind css & launches locally
-rm -rf public static/css
-./tailwindcss -i css/index.css -o ./static/css/index.css --watch & zola serve &
-
 # compiles tailwind css for prod & builds project
-./tailwindcss -i css/index.css -o ./static/css/index.css --minify &
+./tailwindcss -i css/index.css -o ./static/css/index.css --minify
 zola build
 
 
