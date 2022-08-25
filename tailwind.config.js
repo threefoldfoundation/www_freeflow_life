be1a606f2ee0601aded65f31f95b1577721b2b7b
timurgordon <timurgordon@gmail.com>
2022-08-25 16:27:48 +0300

    mobile view fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./templates/**/*.html'],
+  safelist: [
+    "md:-ml-16",
+    "md:-ml-20",
+    "md:-ml-24",
+    "md:-ml-28",
+    {
+        pattern: /-ml-(20|24|28)/,
+        variants: ['sm', 'md', 'lg', 'first:sm', 'first:md', 'first:lg'],
+    },
+    {
+        pattern: /mr-(20|24|28)/,
+        variants: ['sm', 'md', 'lg', 'first:sm', 'first:md', 'first:lg'],
+    },
+    {
+        pattern: /-mr-(20|24|28)/,
+        variants: ['sm', 'md', 'lg', 'last:sm', 'last:md', 'last:lg'],
+    },
+    {
+        pattern: /ml-(20|24|28)/,
+        variants: ['sm', 'md', 'lg', 'last:sm', 'last:md', 'last:lg'],
+    },
+  ],
   theme: {
     extend: {},
   },
